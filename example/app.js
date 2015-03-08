32d40025467e053b91e72136d15b3b4c6246cc0e
Werner Vesteraas <wvesteraas@gmail.com>
2015-03-08 21:54:15 +0100

    Initial commit

---
+++ example/app.js
@@ -0,0 +1,5 @@
+var piinfo = require('../index');
+
+console.log('serial: ' + piinfo.serial());
+console.log('hardware: ' + piinfo.hardware());
+console.log('revision: ' + piinfo.revision());
---
+++ index.js
@@ -0,0 +1,34 @@
+var fs = require('fs');
+
+var info = {};
+
+var lines = fs.readFileSync('/proc/cpuinfo').toString().split('\n');
+for(line in lines) {
+    line = lines[line].replace(/\t/g, '');
+
+    var parts=line.replace(/\t/g, '').split(':');
+
+    if (parts.length == 2) {
+        var key=parts[0];
+        var value=parts[1].trim();
+        if (key === 'Serial') {
+            info.serial = value;
+        } else if (key === 'Hardware') {
+            info.hardware = value;
+        } else if (key === 'Revision') {
+            info.revision = value;
+        }
+    }
+}
+
+module.exports = {
+    serial: function () {
+        return info.serial;
+    },
+    hardware: function() {
+        return info.hardware;
+    },
+    revision: function() {
+        return info.revision;
+    }
+}
